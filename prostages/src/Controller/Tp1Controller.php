38e2f033ad23c4544dc3887c109992b3302db6df
Matias Caturla <mcaturla@iutbayonne-pau.univ.fr>
2021-11-26 09:22:34 +0100

    Création tp1 et entreprise controller

---
+++ prostages/src/Controller/Tp1Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Tp1Controller extends AbstractController
+{
+    /**
+     * @Route("/tp1", name="tp1")
+     */
+    public function index(): Response
+    {
+        return $this->render('tp1/index.html.twig', [
+            'controller_name' => 'Tp1Controller',
+        ]);
+    }
+}

be86cdb2771f30b8031cc49b9a58f59f6ddea855
Matias Caturla <mcaturla@iutbayonne-pau.univ.fr>
2022-01-12 15:22:36 +0100

    ajout fixture2

--- prostages/src/DataFixtures/AppFixtures.php
+++ prostages/src/DataFixtures/AppFixtures.php
@@ -4,12 +4,13 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Entreprise
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
+        // $Airbuse = new Entreprise();
         // $manager->persist($product);
 
         $manager->flush();
